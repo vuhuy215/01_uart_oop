955eea0605da2eaf10037c4fddf8d9ba6d5a8ced
vuhuy <vuhuy2k215@gmail.com>
2022-04-24 08:39:50 +0700

    vu huy update mơi

--- Src/main.c
+++ Src/main.c
@@ -34,6 +34,8 @@ int main(void)
 
 		printf("chuyển sang huyvu\n");
 
+		printf("sửa đổi bằng vuhuy");
+
 
 //		uart_uart1_transmit((uint8_t*)data,strlen(data),HUY_MAX_DELAY);
 		printmsg("hello world \r\n");
